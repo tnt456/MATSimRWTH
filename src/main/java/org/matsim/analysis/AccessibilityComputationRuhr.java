b79771ffd50fc001b971b312577a78e00cdbbe0c
ikaddoura <kaddoura@vsp.tu-berlin.de>
2019-05-22 23:54:09 +0200

    using the plan activities to generate facilities...

--- src/main/java/org/matsim/analysis/AccessibilityComputationRuhr.java
+++ src/main/java/org/matsim/analysis/AccessibilityComputationRuhr.java
@@ -18,9 +18,7 @@
  * *********************************************************************** */
 package org.matsim.analysis;
 
-import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -29,15 +27,18 @@ import org.apache.log4j.Logger;
 import org.locationtech.jts.geom.Envelope;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Plan;
+import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.contrib.accessibility.AccessibilityConfigGroup;
 import org.matsim.contrib.accessibility.AccessibilityConfigGroup.AreaOfAccesssibilityComputation;
-import org.matsim.contrib.accessibility.utils.VisualizationUtils;
 import org.matsim.contrib.accessibility.Modes4Accessibility;
+import org.matsim.contrib.accessibility.utils.VisualizationUtils;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
 import org.matsim.core.config.groups.FacilitiesConfigGroup.FacilitiesSource;
+import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ActivityParams;
 import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
 import org.matsim.core.gbl.MatsimRandom;
 import org.matsim.run.RunRuhrgebietScenario;
@@ -49,71 +50,94 @@ public class AccessibilityComputationRuhr {
 	private static final Logger log = Logger.getLogger( AccessibilityComputationRuhr.class ) ;
 
 	public static void main(String[] args) {
-		// Parameters
 		String configFileName = "scenarios/ruhrgebiet-v1.0-1pct/input/ruhrgebiet-v1.0-1pct.config.xml";
 
 		int tileSize_m = 2000;
 		String accessibilityOutputFolder = "accessibility_essen_" + tileSize_m + "/";
 		Envelope envelope = new Envelope(353400, 370700, 5690500, 5710700); // Essen
+
 //		String accessibilityOutputFolder = "accessibility_ruhrgebiet_" + tileSize_m + "/";
 //		Envelope envelope = new Envelope(310200, 430700, 5676900, 5742200); // Ruhrgebiet
+		
 		boolean createQGisOutput = true;
 
+		// Alternative A: use a facility file which is generated externaly, e.g. based on OSM
 //		String facilitiesFileName = "/Users/dominik/Bicycle/NEMO/facilities/2019-05-08_essen_vicinity.xml.gz";
-		String facilitiesFileName = "../shared-svn/projects/nemo_mercator/data/matsim_input/accessibility/2019-05-08_essen_vicinity.xml.gz";
+//		String facilitiesFileName = "../shared-svn/projects/nemo_mercator/data/matsim_input/accessibility/2019-05-08_essen_vicinity.xml.gz";
 //		String facilitiesFileName = "../shared-svn/projects/nemo_mercator/data/matsim_input/accessibility/ruhrgebiet-v1.0-1pct.output_facilities.xml.gz";
-		final List<String> activityTypes = Arrays.asList(new String[]{"supermarket"});
+//		final List<String> activityTypes = Arrays.asList(new String[]{"supermarket"});
 		
-//		final List<String> activityTypes = new ArrayList<>();
-//		final double minDuration = 600;
-//        final double maxDuration = 3600 * 27;
-//        final double difference = 600;
-//        for (double duration = minDuration; duration <= maxDuration; duration += difference) {
-//            activityTypes.add("work_" + duration);
-//        }
-        
-        for (String act : activityTypes) {
-        	log.info(act);
-        }
+		final List<String> consideredActivityTypePrefixes = new ArrayList<>();
+        consideredActivityTypePrefixes.add("work");
+        consideredActivityTypePrefixes.add("other");
+        consideredActivityTypePrefixes.add("education");
+        consideredActivityTypePrefixes.add("leisure");
 
 		RunRuhrgebietScenario ruhrgebietScenarioRunner = new RunRuhrgebietScenario(new String[]{ "--" + RunRuhrgebietScenario.CONFIG_PATH, configFileName });
 		
 		Config config = ruhrgebietScenarioRunner.prepareConfig();
+
+		// Alternative A: use a facility file which is generated externaly, e.g. based on OSM
+//		config.facilities().setInputFile(new File(facilitiesFileName).getAbsolutePath());
+		
+		// Alternative B: generate facilities based on the activities in the plans file
+		config.facilities().setFacilitiesSource(FacilitiesSource.onePerActivityLocationInPlansFile);
 	
-		config.facilities().setInputFile(new File(facilitiesFileName).getAbsolutePath());
-//		config.facilities().setFacilitiesSource(FacilitiesSource.onePerActivityLocationInPlansFile);
-			
 		config.controler().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
 		config.controler().setFirstIteration(0);
 		config.controler().setLastIteration(0);
-		
 		config.controler().setOutputDirectory(accessibilityOutputFolder);
 		
 		AccessibilityConfigGroup acg = ConfigUtils.addOrGetModule(config, AccessibilityConfigGroup.class);
 		acg.setTileSize_m(tileSize_m);
 		acg.setAreaOfAccessibilityComputation(AreaOfAccesssibilityComputation.fromBoundingBox);
 		acg.setEnvelope(envelope);
-		acg.setComputingAccessibilityForMode(Modes4Accessibility.freespeed, false);
-		acg.setComputingAccessibilityForMode(Modes4Accessibility.car, false);
-		acg.setComputingAccessibilityForMode(Modes4Accessibility.bike, true);
+		acg.setComputingAccessibilityForMode(Modes4Accessibility.freespeed, true); // car freespeed accessibility, should work
+		acg.setComputingAccessibilityForMode(Modes4Accessibility.car, false); // congested car based accessibility, should work as well
+		acg.setComputingAccessibilityForMode(Modes4Accessibility.walk, false); // teleported walk mode, should work as well
+		acg.setComputingAccessibilityForMode(Modes4Accessibility.matrixBasedPt, false); // TODO: check!
+		acg.setComputingAccessibilityForMode(Modes4Accessibility.bike, false); // TODO: doesn't work write now (accessibility = -Infinity)! FIXME!
 		
 		Scenario scenario = ruhrgebietScenarioRunner.prepareScenario();
 		
+		boolean downsampling = true;
 		// down-sampling the scenario
-		final double sample = 0.01;
-		downsample( scenario.getPopulation().getPersons(), sample ) ;
-		config.qsim().setFlowCapFactor( config.qsim().getFlowCapFactor()*sample );
-		config.qsim().setStorageCapFactor( config.qsim().getStorageCapFactor()*sample );
+		if (downsampling) {
+			final double sample = 0.1;
+			downsample( scenario.getPopulation().getPersons(), sample ) ;
+			config.qsim().setFlowCapFactor( config.qsim().getFlowCapFactor()*sample );
+			config.qsim().setStorageCapFactor( config.qsim().getStorageCapFactor()*sample );
+		}
+		
+		String activityConsideredForAccessibilityComputation = "";
+		for (String consideredActivityPrefix : consideredActivityTypePrefixes) {
+			activityConsideredForAccessibilityComputation = activityConsideredForAccessibilityComputation + "_" + consideredActivityPrefix;
+		}
+		
+		for (Person person : scenario.getPopulation().getPersons().values()) {
+			for (Plan plan : person.getPlans()) {
+				for (PlanElement pE : plan.getPlanElements()) {
+					if (pE instanceof Activity) {
+						Activity act = (Activity) pE;
+						for (String consideredActivityTypePrefix : consideredActivityTypePrefixes) {
+							if (act.getType().startsWith(consideredActivityTypePrefix)) {
+								act.setType(activityConsideredForAccessibilityComputation);
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		ActivityParams actParams = new ActivityParams(activityConsideredForAccessibilityComputation);
+		actParams.setTypicalDuration(8 * 3600.); // shouldn't have any effect in the accessibility computation
+		config.planCalcScore().addActivityParams(actParams);
 		
 		org.matsim.core.controler.Controler controler = ruhrgebietScenarioRunner.prepareControler();
 		
-		for (String activityType : activityTypes) {
-			AccessibilityModuleRuhr module = new AccessibilityModuleRuhr();
-			module.setConsideredActivityType(activityType);
-//			module.addAdditionalFacilityData(densityFacilities);
-//			module.setPushing2Geoserver(push2Geoserver);
-			controler.addOverridingModule(module);
-		}
+		AccessibilityModuleRuhr module = new AccessibilityModuleRuhr();
+		module.setConsideredActivityType(activityConsideredForAccessibilityComputation);
+		controler.addOverridingModule(module);
 		
 		ruhrgebietScenarioRunner.run();
 		
@@ -127,7 +151,7 @@ public class AccessibilityComputationRuhr {
 
 			String osName = System.getProperty("os.name");
 			String workingDirectory = config.controler().getOutputDirectory();
-			for (String actType : activityTypes) {
+			for (String actType : consideredActivityTypePrefixes) {
 				String actSpecificWorkingDirectory = workingDirectory + actType + "/";
 				for (Modes4Accessibility mode : acg.getIsComputingMode()) {
 					VisualizationUtils.createQGisOutputGraduatedStandardColorRange(actType, mode.toString(), envelope, workingDirectory,
