ea946be0cdc88a88e0dd12e90fd53dad31670289
tnt456 <tobias.liermann@alumni.fh-aachen.de>
2021-03-05 12:10:08 +0100

    Added a Starting Point for the Network Cleaner

--- src/main/java/org/matsim/rwth/run/RunNetworkCleaner.java
+++ src/main/java/org/matsim/rwth/run/RunNetworkCleaner.java
@@ -1,14 +1,7 @@
 package org.matsim.rwth.run;
 
-import ch.sbb.matsim.routing.pt.raptor.SwissRailRaptorModule;
 import net.bhl.matsim.uam.config.UAMConfigGroup;
-import net.bhl.matsim.uam.dispatcher.UAMManager;
-import net.bhl.matsim.uam.infrastructure.UAMStations;
-import net.bhl.matsim.uam.infrastructure.readers.UAMXMLReader;
-import net.bhl.matsim.uam.qsim.UAMQSimModule;
-import net.bhl.matsim.uam.qsim.UAMSpeedModule;
 import net.bhl.matsim.uam.run.UAMConstants;
-import net.bhl.matsim.uam.run.UAMModule;
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
@@ -16,40 +9,40 @@ import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Leg;
 import org.matsim.contrib.bicycle.BicycleConfigGroup;
-import org.matsim.contrib.bicycle.Bicycles;
 import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
-import org.matsim.contrib.dvrp.run.DvrpModule;
-import org.matsim.contribs.discrete_mode_choice.modules.DiscreteModeChoiceModule;
 import org.matsim.contribs.discrete_mode_choice.modules.config.DiscreteModeChoiceConfigGroup;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigGroup;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryLogging;
 import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.network.algorithms.MultimodalNetworkCleaner;
-import org.matsim.core.network.algorithms.TransportModeNetworkFilter;
+import org.matsim.core.scenario.MutableScenario;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.vehicles.VehicleType;
 import org.matsim.vehicles.VehicleUtils;
 
 import java.util.*;
 
-public class RunRWTHScenario {
+public class RunNetworkCleaner {
 
     private static final Logger log = Logger.getLogger(RunRWTHScenario.class);
     private static UAMConfigGroup uamConfigGroup;
     private static Scenario scenario;
     private static Config config;
+    private static Network network;
 
     public static void main(String[] args) {
 
         config = prepareConfig(args);
         scenario = prepareScenario(config);
-        Controler controler = prepareControler(scenario);
-        VehicleType a = VehicleUtils.getDefaultVehicleType();
-        controler.run();
+        MultimodalNetworkCleaner myCleaner = new MultimodalNetworkCleaner(network);
+        Set<String> modes = new HashSet<>();
+        modes.add(UAMConstants.uam);
+        modes.add("car");
+        myCleaner.run(modes);
+
     }
 
     public static Config prepareConfig(String[] args, ConfigGroup... modules) {
@@ -98,6 +91,7 @@ public class RunRWTHScenario {
 
         scenario = ScenarioUtils.createScenario(config);
         ScenarioUtils.loadScenario(scenario);
+        network = scenario.getNetwork();
 
         // remove route information from population since the base case was calibrated with different network
         // TODO: Re-Calibrate baseCase with new network and currnt pt
@@ -120,84 +114,5 @@ public class RunRWTHScenario {
                 });
 
         return scenario;
-    }
-
-    public static Controler prepareControler(Scenario scenario) {
-
-        /*scenario.getPopulation().getFactory().getRouteFactories().setRouteFactory(DefaultEnrichedTransitRoute.class,
-                new DefaultEnrichedTransitRouteFactory());*/
-
-        Controler controler = new Controler(scenario);
-
-
-        // Initiate Urban Air Mobility XML reading and parsing
-        Network network = controler.getScenario().getNetwork();
-        TransportModeNetworkFilter filter = new TransportModeNetworkFilter(network);
-        Set<String> modes = new HashSet<>();
-        modes.add(UAMConstants.uam);
-        Network networkUAM = NetworkUtils.createNetwork();
-        filter.filter(networkUAM, modes);
-
-        filter = new TransportModeNetworkFilter(network);
-        Set<String> modesCar = new HashSet<>();
-        modesCar.add(TransportMode.car);
-        Network networkCar = NetworkUtils.createNetwork();
-        filter.filter(networkCar, modesCar);
-
-        // set up the UAM infrastructure
-        UAMXMLReader uamReader = new UAMXMLReader(networkUAM);
-
-        uamReader.readFile(ConfigGroup.getInputFileURL(controler.getConfig().getContext(), uamConfigGroup.getInputFile())
-                .getPath().replace("%20", " "));
-        final UAMStations uamStations = new UAMStations(uamReader.getStations(), network);
-        final UAMManager uamManager = new UAMManager(network,uamStations,uamReader.getVehicles());
-
-        // populate UAMManager
-        //uamManager.setStations(uamStations);
-        //uamManager.setVehicles(uamReader.getVehicles());
-        UAMQSimModule uamqSimModule= new UAMQSimModule();
-        //uamQsimModule.provideData();
-
-        // sets transit modules in case of simulating/not pT
-        controler.getConfig().transit().setUseTransit(true);
-        if (true) {
-            controler.addOverridingModule(new SwissRailRaptorModule());
-            //controler.addOverridingModule(new BaselineTransitModule());
-        }
-
-        UAMModule uamModule = new UAMModule();
-        //uamModule.install();
-        controler.addOverridingModule(new DvrpModule());
-        controler.addOverridingModule(uamModule);
-        controler.addOverridingModule(new UAMSpeedModule());
-        //controler.addOverridingModule(new DvrpTravelTimeModule());
-
-        controler.configureQSimComponents(configurator -> {
-            UAMQSimModule.activateModes().configure(configurator);
-        });
-
-        if (!controler.getConfig().transit().isUsingTransitInMobsim())
-            throw new RuntimeException("Public transit will be teleported and not simulated in the mobsim! "
-                    + "This will have a significant effect on pt-related parameters (travel times, modal split, and so on). "
-                    + "Should only be used for testing or car-focused studies with fixed modal split.");
-
-        controler.addOverridingModule(new SwissRailRaptorModule());
-
-        // use the (congested) car travel time for the teleported ride mode
-        controler.addOverridingModule(new AbstractModule() {
-            @Override
-            public void install() {
-                addTravelTimeBinding(TransportMode.ride).to(networkTravelTime());
-                addTravelDisutilityFactoryBinding(TransportMode.ride).to(carTravelDisutilityFactoryKey());
-            }
-        });
-
-        Bicycles.addAsOverridingModule(controler);
-
-        controler.addOverridingModule(new DiscreteModeChoiceModule());
-
-        //Did this for testing?
-
-        return controler;
     }
 }
